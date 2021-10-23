d42c9c7481275cf93c05e8d898d7db90972580ab
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-23 08:38:14 -0300

    SDL_Window novaJanela() implementado

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,28 @@
 // ↓ deve ser movido para outro arquivo
 #include <SDL2/SDL.h>
 
+void iniciaSDL();
+SDL_Window* novaJanela();
+
 int main() {
 	printf("Olá mundo!\n");
 	SDL_Log("Olá SDL2");
+
+	iniciaSDL();
+	SDL_Window *J 	= novaJanela();
+	SDL_Renderer *R	=	NULL;
+
+
 	return 0;
+}
+
+void iniciaSDL() {
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "ERROR: SDL2 couldn't init: %s", SDL_GetError());
+	}
+}
+
+SDL_Window* novaJanela() {
+	return SDL_CreateWindow( "Janela", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		640,480, SDL_WINDOW_SHOWN);
 }
